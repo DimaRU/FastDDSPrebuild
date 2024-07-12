29491d0f5eaf0e08f12ce70284fc170b07d17538
Dmitriy Borovikov <dmitry@borovikov.name>
2024-07-12 12:00:44 +0300

    Build v2.13.6

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "FastDDS",
-                      url: "https://github.com/DimaRU/FastDDSPrebuild/releases/download/v2.14.3/fastdds-v2.14.3.xcframework.zip",
-                      checksum: "eb21d0e2551958d675bafe893ecd00d3979c69bbd137a17deddd9fa4a659e94e")
+                      url: "https://github.com/DimaRU/FastDDSPrebuild/releases/download/v2.13.6/fastdds-v2.13.6.xcframework.zip",
+                      checksum: "3c2fd91ba2a3da24e3782be2926cd2750598a105bd2a20781b875037b966acba")
     ]
 )
